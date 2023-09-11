e6ccf1ab222fe49a318ec1603045264c84e9f11b
Nabanita Sarkar <nabanita.sarkar1418@gmail.com>
2023-09-11 16:53:34 +0530

    still trying to fix typing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path, { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,7 +14,7 @@ export default defineConfig({
       '@/assets': path.resolve(__dirname, './src/assets'),
     },
   },
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/main.tsx'),
